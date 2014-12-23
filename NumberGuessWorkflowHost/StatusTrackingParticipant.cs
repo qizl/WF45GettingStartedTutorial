352a50f86fce2c765924ab95c4c2535114035329
qizl <q@qizl.cn>
2014-12-23 16:40:01 +0800

    -

---
+++ NumberGuessWorkflowHost/StatusTrackingParticipant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Activities.Tracking;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessWorkflowHost
+{
+    class StatusTrackingParticipant : TrackingParticipant
+    {
+        protected override void Track(TrackingRecord record, TimeSpan timeout)
+        {
+            ActivityStateRecord asr = record as ActivityStateRecord;
+
+            if (asr != null)
+            {
+                if (asr.State == ActivityStates.Executing &&
+                    asr.Activity.TypeName == "System.Activities.Statements.WriteLine")
+                {
+                    // Append the WriteLine output to the tracking
+                    // file for this instance
+                    using (StreamWriter writer = File.AppendText(record.InstanceId.ToString()))
+                    {
+                        writer.WriteLine(asr.Arguments["Text"]);
+                        writer.Close();
+                    }
+                }
+            }
+        }
+    }
+}
